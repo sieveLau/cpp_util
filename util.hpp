4baf23fd688ddb9826e83ebf04c1617c247abd15
sieveLau <sievelau@gmail.com>
2021-06-04 17:18:49 +0800

    add std::array

--- util.hpp
+++ util.hpp
@@ -17,5 +17,15 @@ auto to_hex(const unsigned char (&bytes)[N]) -> std::string
     }
     return sout.str();
 }
+template<int N>
+auto to_hex(std::array<unsigned char,N> bytes) -> std::string
+{
+    std::ostringstream sout;
+    sout << std::hex << std::setfill('0');
+    for (long long c : bytes) {
+        sout << std::setw(2) << (long long) c;
+    }
+    return sout.str();
+}
 }
 }
