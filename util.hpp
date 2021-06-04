0b63017c6874bb1d9e36011a62a5e335c86c8552
sieveLau <sievelau@gmail.com>
2021-06-04 17:07:26 +0800

    add to_hex

---
+++ util.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "array.hpp"
+#include <iomanip>
+#include <string>
+#include <sstream>
+
+namespace sieve {
+namespace util {
+template<int N>
+auto to_hex(const unsigned char (&bytes)[N]) -> std::string
+{
+    std::ostringstream sout;
+    sout << std::hex << std::setfill('0');
+    for (long long c : bytes) {
+        sout << std::setw(2) << (long long) c;
+    }
+    return sout.str();
+}
+}
+}
